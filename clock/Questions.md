7c3ff78e874ce8c709ac1e5e0244bde3b60e38f7
Max <maxgustafsson.ugglum@gmail.com>
2022-09-06 14:12:55 +0200

    Finished the lab

--- clock/Questions.md
+++ clock/Questions.md
@@ -1,22 +1,22 @@
 ## Q1. The main method (sketched above) handles user input. What additional thread(s) do you need, beyond this main thread?
 One thread each for handling:
 the time progression.
-the user input. 
-the alarm.
+the user input. (this is the main thread)
+the alarm. (we moved the alarm functionality into the time progression thread)
 
 ## Q2. What common data needs to be shared between threads? Where is the data to be stored?
 ### Hint: introduce a dedicated class for this shared data, as outlined above.
-The Current time, Alarm time, State (alarm on/off, changing time boolean (might be able to cover this with a lock/semaphore), changing alarm time boolean (might be able to cover this with a lock/semaphore))
-## 3. For each of your threads, consider:
+The Current time, Alarm time, State (alarm on/off) and a lock
+## Q3. For each of your threads, consider:
 ### • What operations on shared data are needed for the thread?
 ### • Where in the code is this logic best implemented?
-Time progression: current time, if time is being changed (should not tick while the user is changing time)
-Alarm: current time, alarm time, alarm on/off, changing time boolean, changing alarm time boolean.
-User input: should be able to change current time, change alarm on/off state
+Time progression: current time
+Alarm: current time, alarm time, alarm on/off
+User input: should be able to change current time, change alarm time, change alarm on/off state
 ## Q4. In which parts of your code is data accessed concurrently from different threads? Where in your code do you need to ensure mutual exclusion?
 Time progression accesses and updates current time constantly, and needs to ensure that it has mutual exclusion while that is happening.
-Alarm will be reading current time and alarm time constantly, and needs to ensure that it has mutual exclusion while that is happening.
-User input can change alarm time and current time constantly, and needs to ensure that it has mutual exclusion throughout the entirety of the users interaction, as automatic effects should not happen then.
+Alarm will be reading current time, alarm state and alarm time constantly, and needs to ensure that it has mutual exclusion while that is happening.
+User input can change alarm time, alarm state and current time constantly, and needs to ensure that it has mutual exclusion while that is happening.
 ## Q5. Are there other situations in the alarm clock where semaphores are to be used?
 ### Hint: have a look at ClockInput in section 1.1.2.
 There is a semaphore on userInput to ensure that it does not change as the program is executing based on the user input.
--- clock/src/ClockMain.java
+++ clock/src/ClockMain.java
@@ -1,4 +1,7 @@
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import clock.AlarmClockEmulator;
 import clock.io.ClockInput;
 import clock.io.ClockInput.UserInput;
@@ -7,62 +10,62 @@ import clock.io.ClockOutput;
 public class ClockMain {
 
     public static void main(String[] args) throws InterruptedException {
+        new ClockMain().run(); // To get rid of static requirements for methods that should not require the
+                               // static keyword.
+    }
+
+    void run() throws InterruptedException {
         AlarmClockEmulator emulator = new AlarmClockEmulator();
         ClockInput in = emulator.getInput();
         ClockOutput out = emulator.getOutput();
         Semaphore inputSemaphore = in.getSemaphore();
         clockMonitor monitor = new clockMonitor(0, 0, 0);
-        Thread t1 = new Thread(() -> {
+
+        Thread timeProgressionThread = new Thread(() -> {
             try {
                 timeProgression(monitor, out);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
-        Thread t2 = new Thread(() -> { // Not sure if we need this thread, might be removed later.
-            try {
+        timeProgressionThread.start();
+        while (true) {
+            inputSemaphore.acquire();
+            UserInput userInput = in.getUserInput();
+            int choice = userInput.getChoice();
+            int h = userInput.getHours();
+            int m = userInput.getMinutes();
+            int s = userInput.getSeconds();
+            int[] time = { h, m, s };
+            switch (choice) {
+                case 1: // If the user is setting a new clock time.
+                    changeTime(time, monitor, out);
+                    break;
 
-                while (true) {
-                    inputSemaphore.acquire();
-                    UserInput userInput = in.getUserInput();
-                    int choice = userInput.getChoice();
-                    int h = userInput.getHours();
-                    int m = userInput.getMinutes();
-                    int s = userInput.getSeconds();
-                    int[] time = { h, m, s };
-                    switch (choice) {
-                        case 1: // If the user is setting a new clock time.
-                            changeTime(time, monitor, out);
-                            break;
+                case 2: // If the user is setting a new alarm time.
+                    monitor.setAlarmTime(time);
+                    break;
 
-                        case 2: // If the user is setting a new alarm time.
-                            monitor.setAlarmTime(time);
-                            break;
-
-                        case 3: // If the user wants to toggle the alarm.
-                            out.setAlarmIndicator(monitor.toggleAlarm());
-                            break;
-                    }
-                    System.out.println("choice=" + choice + " h=" + h + " m=" + m + " s=" + s);
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+                case 3: // If the user wants to toggle the alarm.
+                    out.setAlarmIndicator(monitor.toggleAlarm());
+                    break;
             }
-        });
-        t1.start();
-        t2.start();
+            System.out.println("choice=" + choice + " h=" + h + " m=" + m + " s=" + s);
+        }
     }
 
-    static void timeProgression(clockMonitor monitor, ClockOutput out) throws InterruptedException {
+    void timeProgression(clockMonitor monitor, ClockOutput out) throws InterruptedException {
         long t0 = System.currentTimeMillis();
         int counter = 1;
+        int[] time;
         while (true) {
             long now = System.currentTimeMillis();
-            Thread.sleep(counter*1000 - now + t0);
-            int[] time = monitor.getTime();
+            Thread.sleep(counter * 1000 - now + t0);
+            time = monitor.getTime();
             time[2] = time[2] + 1;
             keepTrackOfTimePeriodicity(time);
             changeTime(time, monitor, out);
+            keepTrackOfAlarm(time, monitor, out);
             counter++;
         }
     }
@@ -70,7 +73,7 @@ public class ClockMain {
     /*
      * To handle the periodicity of a 24hr clock.
      */
-    private static void keepTrackOfTimePeriodicity(int[] time) {
+    private void keepTrackOfTimePeriodicity(int[] time) {
         if (time[2] == 60) {
             time[1]++;
             time[2] = 0;
@@ -84,9 +87,21 @@ public class ClockMain {
         }
     }
 
-    private static void changeTime(int[] time, clockMonitor m, ClockOutput out) {
+    private void changeTime(int[] time, clockMonitor m, ClockOutput out) {
         out.displayTime(time[0], time[1], time[2]);
         m.setTime(time);
+    }
+
+    private void keepTrackOfAlarm(int[] time, clockMonitor m, ClockOutput out) {
+        int timeDiff = timeComparison(time, m.getAlarmTime());
+        if (0 <= timeDiff && timeDiff < 20 && m.getAlarmSet()) {
+            out.alarm();
+        }
+    }
+
+    private int timeComparison(int[] currentTime, int[] alarmTime) {
+        return (currentTime[0] * 3600 + currentTime[1] * 60 + currentTime[2])
+                - (alarmTime[0] * 3600 + alarmTime[1] * 60 + alarmTime[2]);
     }
 }
 
@@ -98,10 +113,8 @@ class clockMonitor {
     private int alarmMinute;
     private int alarmSecond;
     private boolean alarmSet;
+    private Lock lock = new ReentrantLock();
 
-    /*
-     * 
-     */
     clockMonitor(int h, int m, int s) {
         this.currentHour = h;
         this.currentMinute = m;
@@ -112,25 +125,46 @@ class clockMonitor {
         alarmSet = false;
     }
 
-    public synchronized void setTime(int[] time) {
+    public void setTime(int[] time) {
+        lock.lock();
         currentHour = time[0];
         currentMinute = time[1];
         currentSecond = time[2];
+        lock.unlock();
     }
 
-    public synchronized void setAlarmTime(int[] time) {
+    public void setAlarmTime(int[] time) {
+        lock.lock();
         alarmHour = time[0];
         alarmMinute = time[1];
         alarmSecond = time[2];
+        lock.unlock();
     }
 
-    public synchronized int[] getTime() {
+    public int[] getTime() {
+        lock.lock();
         int[] result = { currentHour, currentMinute, currentSecond };
+        lock.unlock();
+        return result;
+    }
+
+    public int[] getAlarmTime() {
+        lock.lock();
+        int[] result = { alarmHour, alarmMinute, alarmSecond };
+        lock.unlock();
         return result;
     }
 
     public boolean toggleAlarm() {
+        lock.lock();
         alarmSet = !alarmSet;
+        lock.unlock();
         return alarmSet;
+    }
+
+    public boolean getAlarmSet() {
+        return alarmSet; // Should not require a lock, since it only performs one memory access. Even if
+                         // it happens concurrently with toggle alarm it will either act as though it
+                         // happened before or after.
     }
 }
